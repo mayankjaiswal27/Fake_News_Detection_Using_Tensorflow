a840030d89a1f8ef93037fe7f02be98130aca1dd
mayankjaiswal27 <mayankjaiswal0104@gmail.com>
2025-03-23 17:00:14 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -103,4 +103,5 @@ def analyze_text():
 if __name__ == '__main__':
     if not os.path.exists("uploads"):
         os.makedirs("uploads")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
